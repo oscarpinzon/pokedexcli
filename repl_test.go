e2e79230dee4ddd036a5f962410e925abe7ae516
Oscar Pinzon <oscar.eugenio.pinzon@gmail.com>
2024-01-26 23:23:53 -0500

    add tests

---
+++ repl_test.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"testing"
+)
+
+func TestCleanInput(t *testing.T) {
+	cases := []struct {
+		input    string
+		expected []string
+	}{
+		{
+			input:    " ",
+			expected: []string{},
+		},
+		{
+			input:    "  hello  ",
+			expected: []string{"hello"},
+		},
+		{
+			input:    "  hello  world  ",
+			expected: []string{"hello", "world"},
+		},
+		{
+			input:    "  HellO  World  ",
+			expected: []string{"hello", "world"},
+		},
+	}
+
+	for _, c := range cases {
+		actual := cleanInput(c.input)
+		if len(actual) != len(c.expected) {
+			t.Errorf("Expected %d words, got %d", len(c.expected), len(actual))
+		}
+		for i, word := range actual {
+			if word != c.expected[i] {
+				t.Errorf("Expected %s, got %s", c.expected[i], word)
+			}
+		}
+	}
+}
