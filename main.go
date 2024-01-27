30faf30c5a933f42d9b154577735093f9a4b372a
Oscar Pinzon <oscar.eugenio.pinzon@gmail.com>
2024-01-26 23:00:11 -0500

    change message format

--- main.go
+++ main.go
@@ -30,11 +30,14 @@ func getCommands() map[string]cliCommand {
 
 func commandHelp() error {
 	commands := getCommands()
+	fmt.Println()
 	fmt.Println("Welcome to the Pokedex!")
 	fmt.Println("Usage:")
+	fmt.Println()
 	for _, command := range commands {
-		fmt.Printf("%s - %s\n", command.name, command.description)
+		fmt.Printf("%s: %s\n", command.name, command.description)
 	}
+	fmt.Println()
 	return nil
 }
 
